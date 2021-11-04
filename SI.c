c8fb6b3402189687bf4afe8600708efbbe6fbe1d
Ercan Ersoy <ercanersoy@ercanersoy.net>
2021-11-04 07:03:08 +0300

    Add comments
    Signed-off-by: Ercan Ersoy <ercanersoy@ercanersoy.net>

--- SI.c
+++ SI.c
@@ -8,21 +8,31 @@
 // Convert lower function
 SI_lower SI_convert_lower(long double size, SI_lower_prefix prefix)
 {
+    // Declare the value
     SI_lower value;
 
+    // Calculate the value and assign the size
     value.size = size * pow(10, prefix);
+
+    // Assign the prefix
     value.prefix = prefix;
 
+    // Return the value
     return value;
 }
 
 // Convert upper function
 SI_upper SI_convert_upper(long double size, SI_upper_prefix prefix)
 {
+    // Define the value
     SI_upper value;
 
+    // Calculate the value and assign the size
     value.size = size / pow(10, prefix);
+
+    // Assign the prefix
     value.prefix = prefix;
 
+    // Return the value
     return value;
 }

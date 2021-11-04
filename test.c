d9e8c02cf380e3e2d30fc07058ca523f2ab1165c
Ercan Ersoy <ercanersoy@ercanersoy.net>
2021-11-04 07:06:24 +0300

    Add and update some comments
    Signed-off-by: Ercan Ersoy <ercanersoy@ercanersoy.net>

--- test.c
+++ test.c
@@ -8,77 +8,98 @@
 // Main function
 int main(void)
 {
-    // Define test variables
+    // Define test values
     SI_lower test_lower;
     SI_upper test_upper;
 
-    // Tests
-
+    // Test
     test_lower = SI_convert_lower(123.0, SI_lower_none);
     printf("Value: %.30Lf \n", test_lower.size);
 
+    // Test
     test_lower = SI_convert_lower(123.0, SI_d);
     printf("Value: %.30Lf \n", test_lower.size);
 
+    // Test
     test_lower = SI_convert_lower(123.0, SI_c);
     printf("Value: %.30Lf \n", test_lower.size);
 
+    // Test
     test_lower = SI_convert_lower(123.0, SI_m);
     printf("Value: %.30Lf \n", test_lower.size);
 
+    // Test
     test_lower = SI_convert_lower(123.0, SI_mc);
     printf("Value: %.30Lf \n", test_lower.size);
 
+    // Test
     test_lower = SI_convert_lower(123.0, SI_n);
     printf("Value: %.30Lf \n", test_lower.size);
 
+    // Test
     test_lower = SI_convert_lower(123.0, SI_p);
     printf("Value: %.30Lf \n", test_lower.size);
 
+    // Test
     test_lower = SI_convert_lower(123.0, SI_f);
     printf("Value: %.30Lf \n", test_lower.size);
 
+    // Test
     test_lower = SI_convert_lower(123.0, SI_a);
     printf("Value: %.30Lf \n", test_lower.size);
 
+    // Test
     test_lower = SI_convert_lower(123.0, SI_z);
     printf("Value: %.30Lf \n", test_lower.size);
 
+    // Test
     test_lower = SI_convert_lower(123.0, SI_y);
     printf("Value: %.30Lf \n", test_lower.size);
 
+    // Test
     test_upper = SI_convert_upper(123.0, SI_upper_none);
     printf("Value: %.30Lf \n", test_upper.size);
 
+    // Test
     test_upper = SI_convert_upper(123.0, SI_da);
     printf("Value: %.30Lf \n", test_upper.size);
 
+    // Test
     test_upper = SI_convert_upper(123.0, SI_h);
     printf("Value: %.30Lf \n", test_upper.size);
 
+    // Test
     test_upper = SI_convert_upper(123.0, SI_k);
     printf("Value: %.30Lf \n", test_upper.size);
 
+    // Test
     test_upper = SI_convert_upper(123.0, SI_M);
     printf("Value: %.30Lf \n", test_upper.size);
 
+    // Test
     test_upper = SI_convert_upper(123.0, SI_G);
     printf("Value: %.30Lf \n", test_upper.size);
 
+    // Test
     test_upper = SI_convert_upper(123.0, SI_T);
     printf("Value: %.30Lf \n", test_upper.size);
 
+    // Test
     test_upper = SI_convert_upper(123.0, SI_P);
     printf("Value: %.30Lf \n", test_upper.size);
 
+    // Test
     test_upper = SI_convert_upper(123.0, SI_E);
     printf("Value: %.30Lf \n", test_upper.size);
 
+    // Test
     test_upper = SI_convert_upper(123.0, SI_Z);
     printf("Value: %.30Lf \n", test_upper.size);
 
+    // Test
     test_upper = SI_convert_upper(123.0, SI_Y);
     printf("Value: %.30Lf \n", test_upper.size);
 
+    // Exit
     return 0;
 }
